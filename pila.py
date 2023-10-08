3c8a5cc0d673a0eab246fbe63e54028c866315ec
tomas <tomasmancera@gmail.com>
2023-10-08 15:27:38 -0500

    Testing

---
+++ pila.py
@@ -0,0 +1,98 @@
+import time
+
+
+class nodoPila(object):
+    info, sig = None, None
+
+
+class Pila(object):
+    def __init__(self):
+        self.cima = None
+        self.tamano = 0
+
+
+def apilar(pila, dato):
+    nodo = nodoPila()
+    nodo.info = dato
+    nodo.sig = pila.cima
+    pila.cima = nodo
+    pila.tamano += 1
+
+
+def desapilar(pila):
+    x = pila.cima.info
+    pila.cima = pila.cima.sig
+    pila.tamano -= 1
+    return x
+
+
+def pila_vacia(pila):
+    return pila.cima is None
+
+
+def en_cima(pila):
+    if pila.cima is not None:
+        return pila.cima.info
+    else:
+        return None
+
+
+def tamano(pila):
+    return pila.tamano
+
+
+def barrido(pila):
+    paux = Pila()
+    while (not pila_vacia(pila)):
+        dato = desapilar(pila)
+        print(dato)
+        apilar(paux, dato)
+    while (not pila_vacia(paux)):
+        dato = desapilar(paux)
+        apilar(pila, dato)
+
+
+def verPlaylist(pila):
+    if pila_vacia(pila):
+        print("La playlist está vacía(No hay siguiente canción)")
+    else:
+        print("Discos disponibles:")
+
+    paux = Pila()
+    while not pila_vacia(pila):
+        dato = desapilar(pila)
+        print("\n".join([f"{k}: {v}" for k, v in dato.items()]) + "\n")
+
+        apilar(paux, dato)
+    while not pila_vacia(paux):
+        dato = desapilar(paux)
+        apilar(pila, dato)
+
+
+def seleccionarCancion(pila):
+    if pila_vacia(pila):
+        print("La rockola está vacía(No puedes seleccionar discos)")
+    else:
+        cancion = input("Ingresa el nombre del disco que deseas reproducir: ").capitalize()
+        paux = Pila()
+        cancion_encontrada = False
+
+        while (not pila_vacia(pila)):
+            dato = desapilar(pila)
+
+            if (cancion == dato["NombreDisco"]):
+                print("Disco seleccionado con éxito...")
+                print("Reproduciendo...")
+                time.sleep(dato["Duracion"])
+                print("El disco ha finalizado")
+                cancion_encontrada = True
+
+            apilar(paux, dato)
+
+
+        if not cancion_encontrada:
+            print(f"No se encontró el disco con el nombre {cancion}")
+
+        while (not pila_vacia(paux)):
+            dato = desapilar(paux)
+            apilar(pila, dato)
---
+++ rockola.py
@@ -0,0 +1,30 @@
+from pila import *
+rockola = Pila()
+
+
+x = 0
+while (x != 4):
+    try:
+        x = int(input("---------Elige la opción a ejecutar---------\n1)Agregar disco a la rockola\n2)Seleccionar disco a reproducir\n3)Ver discos en rockola.\n4)Salir\n"))
+    except ValueError:
+        print("El dato ingresado no esta permitido, se esperaba un entero(1-4)")
+    if (x == 1):
+        nombreDisco = input("Escribe el nombre del disco:\n").capitalize()
+        artista = input("Escribe el nombre del Artista:\n").capitalize()
+        duracion = int(input("Ingresa la duracion del disco:\n ---En formato de entero---\n"))
+        disco = {
+            "NombreDisco": nombreDisco,
+            "Artista": artista,
+            "Duracion": duracion
+        }
+        apilar(rockola, disco)
+    elif (x == 2):
+        print("\n===========================================\n")
+        seleccionarCancion(rockola)
+        print("\n===========================================\n")
+    elif (x == 3):
+        print("\n===========================================\n")
+        verPlaylist(rockola)
+        print("\n===========================================\n")
+    elif (x == 4):
+        print("\nEl programa fue finalizado con éxito.\n")
